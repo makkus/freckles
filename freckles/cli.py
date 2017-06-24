49b37398ca19b7204233af8b70bf3ddfab9e2bab
Markus Binsteiner <makkus@posteo.de>
2017-06-24 21:16:50 +0200

    Added profile support

--- freckles/cli.py
+++ freckles/cli.py
@@ -15,8 +15,9 @@ log = logging.getLogger("freckles")
 
 @click.command()
 @click.option('--version', help='the version of freckles you are running', is_flag=True)
+@click.option('--profiles', help='only use one or multiple profiles, not all (if applicable)', multiple=True)
 @click.argument("repo_url", required=True, nargs=1)
-def cli(version, repo_url):
+def cli(version, repo_url, profiles):
     """Freckles manages your dotfiles (and other aspects of your local machine).
 
     For information about how to use and configure Freckles, please visit: XXX
@@ -26,7 +27,7 @@ def cli(version, repo_url):
         click.echo(VERSION)
         sys.exit(0)
 
-    runner = freckles(repo_url)
+    runner = freckles(repo_url, profiles=profiles)
     runner.run(os.path.expanduser("~/.freckles/runs/"), force=True, ansible_verbose="", callback="nsbl_internal")
 
 
--- freckles/freckles.py
+++ freckles/freckles.py
@@ -10,7 +10,7 @@ DEFAULT_ABBREVIATIONS = {
     'bb': ["https://bitbucket.org/", PLACEHOLDER, "/", PLACEHOLDER, ".git"]
 }
 
-def freckles(repo_urls, role_repos=[], task_descs=[], target=None):
+def freckles(repo_urls, profiles=[], role_repos=[], task_descs=[], target=None):
 
         if not target:
             target = os.path.expanduser("~/.nsbl/runs")
@@ -20,7 +20,18 @@ def freckles(repo_urls, role_repos=[], task_descs=[], target=None):
 
         frkl_obj = Frkl(repo_urls, [UrlAbbrevProcessor(init_params={"abbrevs": DEFAULT_ABBREVIATIONS, "add_default_abbrevs": False})])
         expanded_repos = frkl_obj.process()
-        repo_vars = [{"vars": {"freckles_repo": expanded_repos}, "tasks": ["freckles"]}]
+
+        temp = []
+        if profiles:
+                all_profiles = []
+                for p in profiles:
+                        all_profiles.extend(p.split(","))
+                for r in expanded_repos:
+                        temp.append({"repo": r, "profiles": all_profiles})
+        else:
+                temp = expanded_repos
+
+        repo_vars = [{"vars": {"freckles_repo": temp}, "tasks": ["freckles"]}]
 
         nsbl_obj = Nsbl.create(repo_vars, role_repos, task_descs, wrap_into_localhost_env=True, pre_chain=[])
         runner = NsblRunner(nsbl_obj)
