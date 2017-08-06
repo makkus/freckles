e48f7e8197f818bf461c3d89b4813c181ad69c2d
Markus Binsteiner <makkus@posteo.de>
2017-08-06 16:51:28 +0700

    imports

--- freckles/__init__.py
+++ freckles/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-from .commands import FRECKLES_REPO
+from .utils import FRECKLES_REPO, FRECKLES_URL
 
 __author__ = """Markus Binsteiner"""
 __email__ = 'makkus@posteo.net'
