9821a079ece7c32820b07ed7c8e7db511ba95f40
Markus Binsteiner <makkus@posteo.de>
2017-09-11 10:57:03 +0800

    Disabling all tests.

--- tests/test_freckles.py
+++ tests/test_freckles.py
@@ -23,16 +23,19 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    pass
 
 
-# def test_content(response):
-#     """Sample pytest test function with the pytest fixture as an argument.
-#     """
-#     # from bs4 import BeautifulSoup
-#     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    pass
+
 # def test_command_line_interface():
-#     runner = CliRunner()
-#     result = runner.invoke(cli.cli)
-#     help_result = runner.invoke(cli.cli, ['--help'])
-#     assert help_result.exit_code == 0
-#     assert 'Show this message and exit.' in help_result.output
+    # runner = CliRunner()
+    # result = runner.invoke(cli.cli)
+    # help_result = runner.invoke(cli.cli, ['--help'])
+    # assert help_result.exit_code == 0
+    # assert 'Show this message and exit.' in help_result.output
