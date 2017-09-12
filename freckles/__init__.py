615246ea76d8e6dc0bdb3ba28e830c8695daba2d
Markus Binsteiner <makkus@posteo.de>
2017-09-12 15:39:08 +0800

    Bump version: 0.1.116 → 0.1.117

--- freckles/__init__.py
+++ freckles/__init__.py
@@ -7,7 +7,7 @@ import click
 
 __author__ = """Markus Binsteiner"""
 __email__ = 'makkus@posteo.net'
-__version__ = '0.1.116'
+__version__ = '0.1.117'
 
 def print_version(ctx, param, value):
     if not value or ctx.resilient_parsing:
