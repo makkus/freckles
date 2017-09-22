48149ba11f4a9aee074449cbefdb681e8e995503
Markus Binsteiner <makkus@posteo.de>
2017-09-22 12:18:01 +0800

    Changed freckles config file name.

--- freckles/config.py
+++ freckles/config.py
@@ -15,7 +15,7 @@ import yaml
 class FrecklesConfig(object):
     def __init__(self):
 
-        self.config_file = os.path.join(click.get_app_dir('freckles', force_posix=True), 'config.yml')
+        self.config_file = os.path.join(click.get_app_dir('freckles', force_posix=True), FRECKLE_MARKER_FILE_NAME)
         if os.path.exists(self.config_file):
             with open(self.config_file) as f:
                 self.config = yaml.safe_load(f)
--- freckles/freckles_defaults.py
+++ freckles/freckles_defaults.py
@@ -1,6 +1,7 @@
 import os
 from nsbl import defaults
 
+FRECKLE_MARKER_FILE_NAME = ".freckle"
 SUPPORTED_OUTPUT_FORMATS = ["default", "ansible", "skippy", "verbose", "default_full"]
 
 defaults.DEFAULT_ROLES_PATH = os.path.join(os.path.dirname(__file__), "external", "default_role_repo")
