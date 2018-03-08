b5a316c773de597305e840d214ed4dbeb8b7a5dd
Markus Binsteiner <makkus@posteo.de>
2018-03-08 10:13:25 +0700

    Exception handling

--- freckles/profiles.py
+++ freckles/profiles.py
@@ -262,8 +262,7 @@ def assemble_freckle_run(*args, **kwargs):
             result.append(temp)
             click.echo("")
         return result
-    except (Exception) as e:
-        raise e
+    except (Exception, Error) as e:
         click.echo("\nError assembling configuration.\nMessage:  {}\nExiting...".format(e.message))
         sys.exit(1)
 
