64317db58dbb6073e45ba424b81e8c9eb58c7da2
Markus Binsteiner <makkus@posteo.de>
2018-03-29 08:07:57 +1300

    Bump version: 0.5.5 → 0.5.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='freckles',
-    version='0.5.5',
+    version='0.5.6',
     description="DevOps your laptop!",
     long_description=readme + '\n\n' + history,
     author="Markus Binsteiner",
