0c0a2554fb61b61cea32e94cfa10489546bf4d79
Markus Binsteiner <makkus@posteo.de>
2018-04-30 16:32:19 +1200

    Cleanup, moving roles to freckelize.

--- freckles/freckles_dev_cli.py
+++ freckles/freckles_dev_cli.py
@@ -18,7 +18,7 @@ from nsbl import tasks
 from nsbl.defaults import *
 from . import __version__ as VERSION
 from .freckles_defaults import *
-from .utils import get_all_adapters_in_repos, download_extra_repos, DEFAULT_FRECKLES_CONFIG, DEFAULT_ABBREVIATIONS
+from .utils import download_extra_repos, DEFAULT_FRECKLES_CONFIG, DEFAULT_ABBREVIATIONS
 
 
 def reindent(s, numSpaces):
@@ -27,20 +27,27 @@ def reindent(s, numSpaces):
     s = string.join(s, '\n')
     return s
 
+
 def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
     class OrderedLoader(Loader):
         pass
+
     def construct_mapping(loader, node):
         loader.flatten_mapping(node)
         return object_pairs_hook(loader.construct_pairs(node))
+
     OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, construct_mapping)
     return yaml.load(stream, OrderedLoader)
+
 
 def output(python_object, format="raw", pager=False):
     if format == 'yaml':
-        output_string = yaml.safe_dump(python_object, default_flow_style=False, encoding='utf-8', allow_unicode=True)
+        output_string = yaml.safe_dump(
+            python_object,
+            default_flow_style=False,
+            encoding='utf-8',
+            allow_unicode=True)
     elif format == 'json':
         output_string = json.dumps(python_object, sort_keys=4, indent=4)
     elif format == 'raw':
@@ -48,16 +55,25 @@ def output(python_object, format="raw", pager=False):
     elif format == 'pformat':
         output_string = pprint.pformat(python_object)
     else:
-        raise Exception("No valid output format provided. Supported: 'yaml', 'json', 'raw', 'pformat'")
+        raise Exception(
+            "No valid output format provided. Supported: 'yaml', 'json', 'raw', 'pformat'"
+        )
 
     if pager:
         click.echo_via_pager(output_string)
     else:
         click.echo(output_string)
 
+
 @click.group(invoke_without_command=True)
-@click.option('--version', help='the version of frkl you are using', is_flag=True)
-@click.option("--use-repo", "-r", required=False, multiple=True, help="extra context repos to use")
+@click.option(
+    '--version', help='the version of frkl you are using', is_flag=True)
+@click.option(
+    "--use-repo",
+    "-r",
+    required=False,
+    multiple=True,
+    help="extra context repos to use")
 @click.pass_context
 def cli(ctx, version, use_repo):
     """A helper script to make it easier to develop on and with freckles.
@@ -76,7 +92,8 @@ def cli(ctx, version, use_repo):
 
 
 @cli.command('log')
-@click.option('--follow', '-f', help="follow the log file", is_flag=True, default=False)
+@click.option(
+    '--follow', '-f', help="follow the log file", is_flag=True, default=False)
 @click.pass_context
 def log_current(ctx, follow):
     """Prints out the last runs ansible log (verbose).
@@ -86,7 +103,8 @@ def log_current(ctx, follow):
     Instead of manually tailing that particular log-file, this commands lets you do that a tad quicker.
     """
 
-    last_run_folder = os.path.expanduser("~/.local/share/freckles/runs/current")
+    last_run_folder = os.path.expanduser(
+        "~/.local/share/freckles/runs/current")
     last_run_log_folder = os.path.join(last_run_folder, "logs")
 
     ansible_log_file = os.path.join(last_run_log_folder, "ansible_run_log")
@@ -107,7 +125,8 @@ def log_current(ctx, follow):
                 continue
 
             if not f:
-                    f = subprocess.Popen(['tail','-F', ansible_log_file], shell=False)
+                f = subprocess.Popen(
+                    ['tail', '-F', ansible_log_file], shell=False)
 
             time.sleep(1)
             new_link_target = os.readlink(last_run_folder)
@@ -122,19 +141,22 @@ def log_current(ctx, follow):
 def debug_last(ctx):
     """Re-runs the last freckle run directly using ansible-playbook and verbose output."""
 
-    last_run_folder = os.path.expanduser("~/.local/share/freckles/runs/current")
+    last_run_folder = os.path.expanduser(
+        "~/.local/share/freckles/runs/current")
 
     last_run_debug_folder = os.path.join(last_run_folder, "debug")
-    last_run_debug_script = os.path.join(last_run_debug_folder, "debug_all_plays.sh")
+    last_run_debug_script = os.path.join(last_run_debug_folder,
+                                         "debug_all_plays.sh")
 
     run_env = os.environ.copy()
 
     #proc = subprocess.Popen(last_run_debug_script, stdout=subprocess.PIPE, stderr=sys.stdout.fileno(),
-    proc = subprocess.Popen(last_run_debug_script, stdin=subprocess.PIPE, shell=True, env=run_env)
+    proc = subprocess.Popen(
+        last_run_debug_script, stdin=subprocess.PIPE, shell=True, env=run_env)
 
     proc.communicate()
     # for line in iter(proc.stdout.readline, ''):
-        # click.echo(line, nl=False)
+    # click.echo(line, nl=False)
 
 
 def get_all_modules(module_name, module_list):
@@ -143,7 +165,8 @@ def get_all_modules(module_name, module_list):
     module = importlib.import_module(module_name)
 
     prefix = module.__name__ + "."
-    for importer, modname, ispkg in pkgutil.iter_modules(module.__path__, prefix):
+    for importer, modname, ispkg in pkgutil.iter_modules(
+            module.__path__, prefix):
         # print "Found submodule %s (is a package: %s)" % (modname, ispkg)
         if ispkg:
             get_all_modules(modname, module_list)
@@ -152,9 +175,21 @@ def get_all_modules(module_name, module_list):
         # module = __import__(modname, fromlist="dummy")
         # print "Imported", module
 
+
 @cli.command('list-modules')
-@click.option('--filter', '-f', help="filters module names containing this string", required=False, default=None)
-@click.option('--details', '-d', help="print details about matching modules", required=False, default=False, is_flag=True)
+@click.option(
+    '--filter',
+    '-f',
+    help="filters module names containing this string",
+    required=False,
+    default=None)
+@click.option(
+    '--details',
+    '-d',
+    help="print details about matching modules",
+    required=False,
+    default=False,
+    is_flag=True)
 @click.pass_context
 def list_modules_cli(ctx, filter, details):
     """Lists all (default) ansible modules.
@@ -182,11 +217,14 @@ def list_modules_cli(ctx, filter, details):
                 click.echo(dets)
             else:
                 click.secho("{}".format(short_name), bold=True, nl=False)
-                click.echo(": {}".format(module_metadata["doc"]["short_description"]))
+                click.echo(": {}".format(
+                    module_metadata["doc"]["short_description"]))
 
             click.echo("")
         except (ImportError):
-            click.echo("{}: {}".format(short_name, "n/a (probably because of missing python dependencies)"))
+            click.echo("{}: {}".format(
+                short_name,
+                "n/a (probably because of missing python dependencies)"))
 
 
 def list_modules():
@@ -194,6 +232,7 @@ def list_modules():
     module_list = []
     get_all_modules('ansible.modules', module_list)
     return module_list
+
 
 # @cli.command('get-module')
 # @click.argument('module_name', nargs=1)
@@ -244,11 +283,35 @@ def read_module(module_name):
 
     return {"doc": doc, "examples": examples}
 
+
 @cli.command("list-roles")
-@click.option('--filter', '-f', help="filters module names containing this string", required=False, default=None)
-@click.option('--readme', '-r', help="print readme of matching roles", required=False, default=False, is_flag=True)
-@click.option('--defaults', '-d', help="print defaults file of matching roles", required=False, default=False, is_flag=True)
-@click.option('--meta', '-m', help="print meta details of matching roles", required=False, default=False, is_flag=True)
+@click.option(
+    '--filter',
+    '-f',
+    help="filters module names containing this string",
+    required=False,
+    default=None)
+@click.option(
+    '--readme',
+    '-r',
+    help="print readme of matching roles",
+    required=False,
+    default=False,
+    is_flag=True)
+@click.option(
+    '--defaults',
+    '-d',
+    help="print defaults file of matching roles",
+    required=False,
+    default=False,
+    is_flag=True)
+@click.option(
+    '--meta',
+    '-m',
+    help="print meta details of matching roles",
+    required=False,
+    default=False,
+    is_flag=True)
 @click.pass_context
 def list_roles_cli(ctx, filter, readme, defaults, meta):
     """
@@ -259,7 +322,9 @@ def list_roles_cli(ctx, filter, readme, defaults, meta):
 
     config = ctx.obj["config"]
 
-    repos = tasks.get_local_repos(config.trusted_repos, "roles", DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS, DEFAULT_ABBREVIATIONS)
+    repos = tasks.get_local_repos(config.trusted_repos, "roles",
+                                  DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS,
+                                  DEFAULT_ABBREVIATIONS)
 
     click.echo("")
     click.secho("Available roles", bold=True)
@@ -267,7 +332,8 @@ def list_roles_cli(ctx, filter, readme, defaults, meta):
     click.echo("")
 
     for repo in repos:
-        for role, role_details in sorted(tasks.get_role_details_in_repo(repo).items()):
+        for role, role_details in sorted(
+                tasks.get_role_details_in_repo(repo).items()):
 
             if filter and filter not in role:
                 continue
@@ -275,8 +341,10 @@ def list_roles_cli(ctx, filter, readme, defaults, meta):
             click.secho("\n{}\n{}\n".format(role, "-" * len(role)), bold=True)
 
             _path = role_details["path"]
-            _author = role_details["meta"].get("galaxy_info", {}).get("author", "n/a")
-            _desc = role_details["meta"].get("galaxy_info", {}).get("description", "n/a")
+            _author = role_details["meta"].get("galaxy_info", {}).get(
+                "author", "n/a")
+            _desc = role_details["meta"].get("galaxy_info", {}).get(
+                "description", "n/a")
             _role_repo = role_details["repo"]
 
             click.secho("  desc", nl=False, bold=True)
@@ -313,7 +381,8 @@ def list_roles_cli(ctx, filter, readme, defaults, meta):
                 click.echo("")
                 click.secho("  meta", bold=True)
                 click.echo("")
-                yaml_string = yaml.dump(role_details["meta"], default_flow_style=False)
+                yaml_string = yaml.dump(
+                    role_details["meta"], default_flow_style=False)
                 indented = reindent(yaml_string, 4)
                 click.echo(indented)
 
@@ -321,8 +390,19 @@ def list_roles_cli(ctx, filter, readme, defaults, meta):
 
 
 @cli.command("list-blueprints")
-@click.option('--filter', '-f', help="filters blueprints names containing this string", required=False, default=None)
-@click.option('--details', '-d', help="print details about matching blueprints", required=False, default=False, is_flag=True)
+@click.option(
+    '--filter',
+    '-f',
+    help="filters blueprints names containing this string",
+    required=False,
+    default=None)
+@click.option(
+    '--details',
+    '-d',
+    help="print details about matching blueprints",
+    required=False,
+    default=False,
+    is_flag=True)
 @click.pass_context
 def list_blueprints_cli(ctx, filter, details):
     """
@@ -331,7 +411,9 @@ def list_blueprints_cli(ctx, filter, details):
 
     config = ctx.obj["config"]
 
-    repos = tasks.get_local_repos(config.trusted_repos, "blueprints", DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS, DEFAULT_ABBREVIATIONS)
+    repos = tasks.get_local_repos(config.trusted_repos, "blueprints",
+                                  DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS,
+                                  DEFAULT_ABBREVIATIONS)
 
     click.echo("")
     click.secho("Available blueprints", bold=True)
@@ -348,7 +430,8 @@ def list_blueprints_cli(ctx, filter, details):
 
     for name, path in sorted(blueprints.items()):
 
-        metadata = get_blueprint_metadata(os.path.join(path, "{}.blueprint.freckle".format(name)))
+        metadata = get_blueprint_metadata(
+            os.path.join(path, "{}.blueprint.freckle".format(name)))
 
         short_help = metadata.get("doc", {}).get("short_help", "n/a")
         long_help = metadata.get("doc", {}).get("help", "n/a")
@@ -366,12 +449,23 @@ def list_blueprints_cli(ctx, filter, details):
             indented = reindent(long_help, 4)
             click.echo(indented)
 
-
     click.echo("")
 
+
 @cli.command("list-adapters")
-@click.option('--filter', '-f', help="filters adapters names containing this string", required=False, default=None)
-@click.option('--details', '-d', help="print details about matching adapters", required=False, default=False, is_flag=True)
+@click.option(
+    '--filter',
+    '-f',
+    help="filters adapters names containing this string",
+    required=False,
+    default=None)
+@click.option(
+    '--details',
+    '-d',
+    help="print details about matching adapters",
+    required=False,
+    default=False,
+    is_flag=True)
 @click.pass_context
 def list_adapters_cli(ctx, filter, details):
     """
@@ -380,7 +474,9 @@ def list_adapters_cli(ctx, filter, details):
 
     config = ctx.obj["config"]
 
-    repos = tasks.get_local_repos(config.trusted_repos, "adapters", DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS, DEFAULT_ABBREVIATIONS)
+    repos = tasks.get_local_repos(config.trusted_repos, "adapters",
+                                  DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS,
+                                  DEFAULT_ABBREVIATIONS)
 
     click.echo("")
     click.secho("Available adapters", bold=True)
@@ -388,7 +484,8 @@ def list_adapters_cli(ctx, filter, details):
 
     adapters = get_all_adapters_in_repos(repos)
 
-    adapter_files = find_adapter_files(ADAPTER_MARKER_EXTENSION, adapters, config=config)
+    adapter_files = find_adapter_files(
+        ADAPTER_MARKER_EXTENSION, adapters, config=config)
 
     for adapter_name, adapter_file in sorted(adapter_files.items()):
 
@@ -404,13 +501,15 @@ def list_adapters_cli(ctx, filter, details):
 
     click.echo("")
 
+
 def output_adapter_help(adapter_name, adapter_file, details=False, help=False):
 
     metadata = get_adapter_metadata(adapter_file)
     short_help = metadata.get("doc", {}).get("short_help", "n/a")
 
     help_string = metadata.get("doc", {}).get("help", short_help)
-    click.secho("\n{}\n{}\n".format(adapter_name, "-" * len(adapter_name)), bold=True)
+    click.secho(
+        "\n{}\n{}\n".format(adapter_name, "-" * len(adapter_name)), bold=True)
     click.secho("  desc", nl=False, bold=True)
     click.echo(": {}".format(short_help))
     click.secho("  path", nl=False, bold=True)
@@ -458,15 +557,19 @@ def print_adapter_help(ctx, adapter_name):
 
     config = ctx.obj["config"]
 
-    repos = tasks.get_local_repos(config.trusted_repos, "adapters", DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS, DEFAULT_ABBREVIATIONS)
+    repos = tasks.get_local_repos(config.trusted_repos, "adapters",
+                                  DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS,
+                                  DEFAULT_ABBREVIATIONS)
 
     adapters = get_all_adapters_in_repos(repos)
 
-    adapter_files = find_adapter_files(ADAPTER_MARKER_EXTENSION, adapters, config=config)
+    adapter_files = find_adapter_files(
+        ADAPTER_MARKER_EXTENSION, adapters, config=config)
 
     adapter_file = adapter_files.get(adapter_name, False)
     if not adapter_file:
-        click.echo("\nNo adapter with the name '{}' found.\n".format(adapter_name))
+        click.echo(
+            "\nNo adapter with the name '{}' found.\n".format(adapter_name))
         sys.exit(0)
 
     output_adapter_help(adapter_name, adapter_file, details=True, help=True)
@@ -484,6 +587,7 @@ def get_adapter_metadata(adapter_file):
 
     return metadata
 
+
 def get_blueprint_metadata(blueprint_file):
 
     with open(blueprint_file, 'r') as f:
@@ -495,10 +599,20 @@ def get_blueprint_metadata(blueprint_file):
     return metadata
 
 
-
 @cli.command("list-aliases")
-@click.option("--filter", "-f", help="filter aliases contains the provided string", required=False, default=None)
-@click.option('--details', '-d', help="print alias details", required=False, default=False, is_flag=True)
+@click.option(
+    "--filter",
+    "-f",
+    help="filter aliases contains the provided string",
+    required=False,
+    default=None)
+@click.option(
+    '--details',
+    '-d',
+    help="print alias details",
+    required=False,
+    default=False,
+    is_flag=True)
 @click.pass_context
 def list_aliases_cli(ctx, filter, details):
     """
@@ -506,9 +620,12 @@ def list_aliases_cli(ctx, filter, details):
     """
 
     config = ctx.obj["config"]
-    role_repos = tasks.get_local_repos(config.trusted_repos, "roles", DEFAULT_LOCAL_REPO_PATH_BASE, DEFAULT_REPOS, DEFAULT_ABBREVIATIONS)
+    role_repos = tasks.get_local_repos(config.trusted_repos, "roles",
+                                       DEFAULT_LOCAL_REPO_PATH_BASE,
+                                       DEFAULT_REPOS, DEFAULT_ABBREVIATIONS)
 
-    task_descs = calculate_task_descs(None, role_repos, add_upper_case_versions=False)
+    task_descs = calculate_task_descs(
+        None, role_repos, add_upper_case_versions=False)
 
     desc = {}
     aliases = {}
@@ -534,7 +651,11 @@ def list_aliases_cli(ctx, filter, details):
         if details:
             # vars = d.get(VARS_KEY)
             # click.echo("\nalias '{}' (type: {}):".format(name, task_type))
-            yaml_string = yaml.safe_dump(d, default_flow_style=False, allow_unicode=True, encoding="utf-8")
+            yaml_string = yaml.safe_dump(
+                d,
+                default_flow_style=False,
+                allow_unicode=True,
+                encoding="utf-8")
             desc[name]["details"] = yaml_string
             #click.echo("   " + yaml_string
 
@@ -544,11 +665,13 @@ def list_aliases_cli(ctx, filter, details):
     click.echo("")
     for name, d in sorted(desc.items()):
         click.secho("{}".format(name), bold=True, nl=False)
-        click.secho(": {} {}".format(d["task_type"], d["task_name"]), bold=False)
+        click.secho(
+            ": {} {}".format(d["task_type"], d["task_name"]), bold=False)
         if details:
             click.echo("")
             click.echo("   " + d["details"].replace("\n", "\n    "))
     click.echo("")
+
 
 if __name__ == '__main__':
     cli()
