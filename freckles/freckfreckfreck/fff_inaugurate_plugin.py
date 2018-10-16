712bdddbfe9f9bebd9ef049ee4752d3486dc5334
Markus Binsteiner <makkus@frkl.io>
2018-10-16 14:45:26 +0200

    Minor

--- freckles/freckfreckfreck/fff_inaugurate_plugin.py
+++ freckles/freckfreckfreck/fff_inaugurate_plugin.py
@@ -1,68 +1,30 @@
 from __future__ import absolute_import, division, print_function
 
-import importlib
-import os
-import sys
-
 import click
-from plumbum import local, CommandNotFound
-from ruamel.yaml import YAML
 
 # from . import __version__ as VERSION
-from freckles.exceptions import FrecklesException
 
 TEMPLATE_METADATA_SCHEMA = {
-    "exec": {
-        "type": "boolean"
-    },
-    "pip_extra_url": {
-        "type": "string"
-    },
-    "prefix_command": {
-        "type": "string"
-    },
-    "template_init_function": {
-        "type": "string"
-    },
+    "exec": {"type": "boolean"},
+    "pip_extra_url": {"type": "string"},
+    "prefix_command": {"type": "string"},
+    "template_init_function": {"type": "string"},
     "default_profile": {
         "type": "dict",
         "schema": {
-            "profile_name": {
-                "type": "string"
-            },
-            "profile_env_name": {
-                "type": "string"
-            },
-            "conda_python_version": {
-                "type": "string"
-            },
-            "conda_dependencies": {
-                "type": "string"
-            },
-            "executables_to_link": {
-                "type": "list",
-                "schema": {"type": "string"}
-            },
-            "extra_executables": {
-                "type": "list",
-                "schema": {"type": "string"}
-            },
-            "debian_dependencies": {
-                "type": "list",
-                "schema": {"type": "string"}
-            },
-            "rpm_dependencies": {
-                "type": "list",
-                "schema": {"type": "string"}
-            },
-            "pip_dependencies": {
-                "type": "list",
-                "schema": {"type": "string"}
-            },
-
-        }
-    }
+            "profile_name": {"type": "string"},
+            "profile_env_name": {"type": "string"},
+            "conda_python_version": {"type": "string"},
+            "conda_dependencies": {"type": "string"},
+            "executables_to_link": {"type": "list", "schema": {"type": "string"}},
+            "extra_executables": {"type": "list", "schema": {"type": "string"}},
+            "debian_dependencies": {"type": "list", "schema": {"type": "string"}},
+            "rpm_dependencies": {"type": "list", "schema": {"type": "string"}},
+            "pip_dependencies": {"type": "list", "schema": {"type": "string"}},
+        },
+    },
 }
+
 
 @click.command("template")
 @click.argument("metadata", metavar="METADATA", nargs=1, required=True)
@@ -79,20 +41,18 @@ def template(ctx, metadata):
             "profile_name": "freckles",
             "profile_env_name": "freckles",
             "conda_python_version": "2.7",
-            "conda_dependencies": [
-                "git"
-            ],
+            "conda_dependencies": ["git"],
             "executables_to_link": [
                 "freckles",
                 "frecklecute",
                 "freckelize",
-                "freckfreckfreck"
+                "freckfreckfreck",
             ],
             "extra_executables": [
                 "ansible",
                 "ansible-playbook",
                 "ansible-galaxy",
-                "git"
+                "git",
             ],
             "debian_dependencies": [
                 "curl",
@@ -103,7 +63,7 @@ def template(ctx, metadata):
                 "python-virtualenv",
                 "virtualenv",
                 "libssl-dev",
-                "libffi-dev"
+                "libffi-dev",
             ],
             "rpm_dependencies": [
                 "wget",
@@ -113,12 +73,13 @@ def template(ctx, metadata):
                 "openssl-devel",
                 "gcc",
                 "libffi-devel",
-                "python-devel"
+                "python-devel",
             ],
             "pip_dependencies": [
                 "freckles>=0.6.0",
-                "freckles-connector-nsbl[ansible]==0.1.0"
-            ]
-        }
+                "freckles-connector-nsbl[ansible]==0.1.0",
+            ],
+        },
     }
 
+    click.echo(metadata)
