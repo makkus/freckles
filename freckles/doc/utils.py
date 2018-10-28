092c4ce391a62e0ba2c7f9b5827ae9dd5b18653e
Markus Binsteiner <makkus@frkl.io>
2018-10-28 21:22:43 +0100

    Better task info display.

--- freckles/doc/utils.py
+++ freckles/doc/utils.py
@@ -1,6 +1,41 @@
 # -*- coding: utf-8 -*-
 
+from frutils import JINJA_DELIMITER_PROFILES
 from plumbum import local
+from jinja2 import PackageLoader
+from jinja2.nativetypes import NativeEnvironment
+from six import string_types
+
+DOC_TEMPLATE_LOADER = PackageLoader("freckles", "templates")
+DOC_JINJA_ENV = NativeEnvironment(
+    loader=DOC_TEMPLATE_LOADER, **JINJA_DELIMITER_PROFILES["documentation"]
+)
+
+
+def sanitize_rst_filter(value):
+
+    if not isinstance(value, string_types):
+        return value
+
+    if value.endswith("_"):
+        value = "{}\_".format(value[0:-1])
+    return value
+
+
+def make_sentence_filter(value):
+
+    if not isinstance(value, string_types):
+        return value
+
+    if not value.endswith("."):
+        value = value + "."
+
+    value = value.capitalize()
+    return value
+
+
+DOC_JINJA_ENV.filters["sanitize_rst"] = sanitize_rst_filter
+DOC_JINJA_ENV.filters["make_sentence"] = make_sentence_filter
 
 
 def get_frecklecute_help_text():
@@ -12,3 +47,16 @@ def get_frecklecute_help_text():
     rc, stdout, stderr = cmd.run(args, retcode=None)
 
     return stdout
+
+
+def render_frecklet_to_rst(
+    frecklet_name, frecklet, show=None, template_name="frecklet_template_default.rst.j2"
+):
+
+    if show is None:
+        show = {"arguments": False, "references": False, "arg_list_filter": []}
+
+    repl_dict = {"frecklet_name": frecklet_name, "frecklet": frecklet, "show": show}
+    template = DOC_JINJA_ENV.get_template(template_name)
+    rendered = template.render(**repl_dict)
+    return rendered
