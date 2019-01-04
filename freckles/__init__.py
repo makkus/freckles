5c43233e3d9025a42db115982c78d0167eb85e94
Markus Binsteiner <makkus@frkl.io>
2019-01-04 15:38:23 +0100

    Package imports

--- freckles/__init__.py
+++ freckles/__init__.py
@@ -9,6 +9,8 @@ __email__ = "makkus@frkl.io"
 __version__ = "1.0.0b1"
 
 import click
+from .freckles import Freckles   # noqa
+from .frecklet import Frecklet   # noqa
 
 
 def print_version(ctx, param, value):
