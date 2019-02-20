e7c642f2be8133b6666c067d37544e74bdf3db45
Markus Binsteiner <makkus@frkl.io>
2019-02-20 16:40:47 +0100

    Dependencies

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    # "luci==1.0.0b1",
+    "frutils==1.0.0b1",
     "colorama==0.4.1",
     "termcolor==1.1.0",
     "terminaltables==3.1.0",
