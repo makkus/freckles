08cc0de59fa7edcca8027999b2745d0d1631d285
Markus Binsteiner <makkus@frkl.io>
2019-05-03 16:24:10 +0800

    Added setuptools-scm-git-archive plugin

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from pkg_resources import VersionConflict, require
 from setuptools import setup
 
 try:
-    require("setuptools>=38.3")
+    require("setuptools>=38.3", "setuptools_scm_git_archive>=1.1")
 except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
